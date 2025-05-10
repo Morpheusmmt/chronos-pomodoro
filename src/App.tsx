e84b88bb083f1a4318d8977af5ee27cf4c5b4c17
Morpheusmmt <1maida.martins@gmail.com>
2025-05-10 20:15:14 -0300

    criação de novos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import './styles/theme.css';
-import './styles/global.css';
+
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
+import './styles/global.css'
+import './styles/theme.css'
+import { Menu } from './components/Menu';
 
 export function App() {
   return (
     <>
       <Container >
-        <Heading>LOGO</Heading>
+        <Logo />
         </Container>
 
       <Container >
-            <Heading>MENU</Heading>
+        <Menu />
           </Container>
     </>
   );
