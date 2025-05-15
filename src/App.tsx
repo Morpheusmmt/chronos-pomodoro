98e813c580a8ee2e7562cb49c773fe923968d5c1
Morpheusmmt <1maida.martins@gmail.com>
2025-05-15 18:56:45 -0300

    criação do defaultInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
-
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
-import './styles/global.css'
-import './styles/theme.css'
+import './styles/global.css';
+import './styles/theme.css';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
     <>
-      <Container >
+      <Container>
         <Logo />
-        </Container>
+      </Container>
 
-      <Container >
+      <Container>
         <Menu />
-          </Container>
+      </Container>
+      <Container>
+        <CountDown />
+      </Container>
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput labelText='task' id='input' type='text' placeholder='Digite Algo'/>
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+           <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
