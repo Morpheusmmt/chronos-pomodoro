65653ec17f2facad5993a16af8f3d0ff3d2174c2
Morpheusmmt <1maida.martins@gmail.com>
2025-08-31 15:01:27 -0300

    fix: aprimora acessibilidade do componente Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,10 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
 import './styles/global.css';
 import './styles/theme.css';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
+import { Home } from './ Pages/Home';
 
 
 export function App() {
   return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <CountDown />
-      </Container>
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              labelText='task'
-              id='input'
-              type='text'
-              placeholder='Digite Algo'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className='formRow'>
-          <Cycles/>
-          </div>
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} color='green'/>
-          </div>
-        </form>
-      </Container>
-
-       <Container>
-        <Footer />
-      </Container>
-    </>
+   <Home/>
   );
 }
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,48 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon, MoonIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useState, useEffect } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+const [theme, setTheme] = useState<AvailableThemes>(() => {
+  const savedTheme = (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+  return savedTheme;
+});
+
+const nextThemeIcon = {
+  dark: <SunIcon />,
+  light: <MoonIcon />,
+};
+
+function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>){
+ event.preventDefault(); 
+
+  setTheme(prevTheme=> {
+    const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+    return nextTheme;
+  });
+}
+
+useEffect(() => {
+  document.documentElement.setAttribute('data-theme', theme);
+  localStorage.setItem('theme', theme);
+
+}, [theme]);
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href='#'>
+      <a className={styles.menuLink} href='#' aria-Label='Ir para a página inicial' title='Ir para a página inicial'>
         <HouseIcon /> 
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a className={styles.menuLink} href='#' arial-Label='Ver histórico' title='Ver histórico'>
         <HistoryIcon /> 
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a className={styles.menuLink} href='#' aria-Label='Configurações' title='Configurações'>
         <SettingsIcon /> 
       </a>
-      <a className={styles.menuLink} href='#'>
-        <SunIcon /> 
+      <a className={styles.menuLink} href='#' aria-Label='Mudar tema' title='Mudar tema' onClick={(event) =>handleThemeChange (event)}>
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
